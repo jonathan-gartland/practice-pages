08091c887fe3803fe3214cba44ede4fa269576ec
jonathan-gartland <jonathan-gartland@gmail.com>
2023-12-04 11:22:14 -0500

    Updates

---
+++ precache-manifest.08ee5bf6efefc2138ed013803d3853ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2803d386db83976f29685c6f5f1f197",
+    "url": "/practice-pages/index.html"
+  },
+  {
+    "revision": "0055e79ea2e86c27a127",
+    "url": "/practice-pages/static/css/main.05a89493.chunk.css"
+  },
+  {
+    "revision": "185b8a5411cdbce8aebf",
+    "url": "/practice-pages/static/js/2.0f7330a7.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/practice-pages/static/js/2.0f7330a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0f0d41f70104d991483",
+    "url": "/practice-pages/static/js/3.123cb99e.chunk.js"
+  },
+  {
+    "revision": "0055e79ea2e86c27a127",
+    "url": "/practice-pages/static/js/main.a1bfe77e.chunk.js"
+  },
+  {
+    "revision": "8d7047d1cf26a466e067",
+    "url": "/practice-pages/static/js/runtime-main.841d2e42.js"
+  }
+]);
